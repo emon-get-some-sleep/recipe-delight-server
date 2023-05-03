61968057a8d8201df816c24f455ddf97943603a8
emonh0que <you@example.com>
2023-05-03 16:18:26 +0600

    added json data about chef, and foodGallary

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
 const app = express();
-
+var cors = require('cors')
+app.use(cors());
+const chef = require('./chef.json');
+const foodGallary  = require('./foodGallary.json');
 app.get('/', (req, res) => {
     res.send('Recepi Delight is online');
 })
+
+app.get('/chefs', (req, res) => {
+    res.send(chef);
+})
+
+app.get('/foodGallary', (req, res) => {
+    res.send(foodGallary);
+})
+
+
 
 app.listen(5000, () => {
     console.log('server is running on port 5000')
